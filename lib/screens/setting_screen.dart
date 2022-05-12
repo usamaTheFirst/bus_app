a0bcbbbb6e22716d7b20e05c775ce5df9c84d357
Usama <72226908+usamaTheFirst@users.noreply.github.com>
2022-05-12 11:47:20 +0500

    made tweaks to update password file

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bus_ticket_app/exports.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -29,124 +30,129 @@ class _SettingScreenState extends State<SettingScreen> {
         title: const Text('Setting'),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Flexible(
-                    child: TextFormField(
-                      readOnly: nameEditable,
-                      initialValue: data.name,
-                      onChanged: (value) => name = value,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  nameEditable
-                      ? ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: kPrimaryColor,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              nameEditable = false;
-                            });
-                          },
-                          child: const Text('Edit'))
-                      : ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: kPrimaryColor,
-                          ),
-                          onPressed: () {
-                            setState(() async {
-                              final user = FirebaseAuth.instance.currentUser;
-                              await user?.updateDisplayName(name);
-                              await user?.reload();
-                              setState(() {});
-                            });
-                          },
-                          child: const Text('Confirm'))
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            if (editPassword)
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: kPrimaryColor,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      editPassword = false;
-                    });
-                  },
-                  child: const Text("Change Password")),
-            if (!editPassword)
-              Form(
-                key: _formKey,
-                child: Column(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    TextFormField(
-                      decoration:
-                          const InputDecoration(labelText: 'Old Password'),
-                      onChanged: (value) => oldPassword = value,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter your old password';
-                        }
-                        return null;
-                      },
+                    Flexible(
+                      child: TextFormField(
+                        readOnly: nameEditable,
+                        initialValue: data.name,
+                        onChanged: (value) => name = value,
+                      ),
                     ),
                     const SizedBox(
-                      height: 30,
+                      width: 30,
                     ),
-                    TextFormField(
-                      decoration:
-                          const InputDecoration(labelText: 'New Password'),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter your old password';
-                        } else if (value.length < 7) {
-                          return 'Password must be at least 7 characters';
-                        } else if (newPassword == oldPassword) {
-                          return "New password can't be the same as old password";
-                        }
-                        return null;
-                      },
-                      onChanged: (value) => newPassword = value,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: kPrimaryColor,
-                      ),
-                      onPressed: () async {
-                        setState(() {
-                          passwordLoader = true;
-                        });
-                        _formKey.currentState!.validate();
-                        await _changePassword(oldPassword, newPassword);
-
-                        setState(() {
-                          passwordLoader = false;
-                        });
-                      },
-                      child: passwordLoader
-                          ? const CircularProgressIndicator(
-                              color: Colors.red,
-                            )
-                          : const Text("Confirm"),
-                    ),
+                    nameEditable
+                        ? ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: kPrimaryColor,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                nameEditable = false;
+                              });
+                            },
+                            child: const Text('Edit'))
+                        : ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: kPrimaryColor,
+                            ),
+                            onPressed: () {
+                              setState(() async {
+                                final user = FirebaseAuth.instance.currentUser;
+                                await user?.updateDisplayName(name);
+                                await user?.reload();
+                                setState(() {});
+                              });
+                            },
+                            child: const Text('Confirm'))
                   ],
                 ),
               ),
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              if (editPassword)
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: kPrimaryColor,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        editPassword = false;
+                      });
+                    },
+                    child: const Text("Change Password")),
+              if (!editPassword)
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        obscureText: true,
+                        decoration:
+                            const InputDecoration(labelText: 'Old Password'),
+                        onChanged: (value) => oldPassword = value,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your old password';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration:
+                            const InputDecoration(labelText: 'New Password'),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your old password';
+                          } else if (value.length < 7) {
+                            return 'Password must be at least 7 characters';
+                          } else if (newPassword == oldPassword) {
+                            return "New password can't be the same as old password";
+                          }
+                          return null;
+                        },
+                        onChanged: (value) => newPassword = value,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: kPrimaryColor,
+                        ),
+                        onPressed: () async {
+                          setState(() {
+                            passwordLoader = true;
+                          });
+                          _formKey.currentState!.validate();
+                          await _changePassword(oldPassword, newPassword);
+
+                          setState(() {
+                            passwordLoader = false;
+                          });
+                        },
+                        child: passwordLoader
+                            ? const CircularProgressIndicator(
+                                color: Colors.red,
+                              )
+                            : const Text("Confirm"),
+                      ),
+                    ],
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
